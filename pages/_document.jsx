cb45d8d6a34eb94f78c2ca6cd0d52873eca765f1
Aman Kumar <amantheroot@gmail.com>
2020-11-01 22:30:30 +0530

    added next js

---
+++ pages/_document.jsx
@@ -0,0 +1,44 @@
+import Document, { Head, Main, NextScript } from "next/document";
+
+const APP_NAME = "MDMA";
+const APP_DESCRIPTION = "MDMA. Not the drug.";
+const APP_THEME_COLOR = "#000000";
+
+export default class extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <html lang="en" dir="ltr">
+        <Head>
+          <meta name="application-name" content={APP_NAME} />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta
+            name="apple-mobile-web-app-status-bar-style"
+            content="default"
+          />
+          <meta name="apple-mobile-web-app-title" content={APP_NAME} />
+          <meta name="description" content={APP_DESCRIPTION} />
+          <meta name="format-detection" content="telephone=no" />
+          <meta name="mobile-web-app-capable" content="yes" />
+          <meta name="theme-color" content={APP_THEME_COLOR} />
+
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/icons/apple-touch-icon.png"
+          />
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="shortcut icon" href="/icons/favicon.ico" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
---
+++ pages/index.jsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import "../styles/styles.scss";
+
+const IndexPage = () => {
+  return (
+    <>
+      <Head>
+        <meta name="description" content="MDMA. Not the drug." />
+        <title>Modern Day Marketing Agency | MDMA</title>
+      </Head>
+      <h1>MDMA</h1>
+    </>
+  );
+};
+
+export default IndexPage;
